52aeb6779277eadcd2277f7d998c8e63c087e28c
Emil Karlsson <emil.axel.karlsson@gmail.com>
2026-02-28 23:45:34 +0100

    Velocity calculation working

--- fatar88key.ino
+++ fatar88key.ino
@@ -45,8 +45,11 @@ const uint8_t matrix[16][16] = {
     {31, 30, 29, 28, 27, 26, 25, 24, 71, 70, 69, 68, 67, 66, 65, 64}
 };
 
+const uint32_t NOPE_ITS_RELEASED = 0;
+const uint32_t KEYBED_OFFSET_RIGHT = 9; 
+
 // 0 means it is released
-uint32_t micros_at_partial[88] = { 0 };
+uint32_t micros_at_partial[88] = { NOPE_ITS_RELEASED };
 
 uint8_t in_flight_velocities[88] = { 0 };
 
@@ -106,12 +109,17 @@ unsigned int readInputs() {
   return r;
 }
 
+uint8_t velocity_from_press_micros(uint32_t us) {
+  int8_t vel = 127 - min(127, (127 * us) / 100000);
+  return max(0, (uint8_t)vel);
+}
+
 void setup() {
   // Set MIDI baud rate:
   Serial.begin(115200);
 
   pinMode(PIN_SO, OUTPUT);
-  pinMode(PIN_SI, INPUT_PULLUP); // Does pull matter..?
+  pinMode(PIN_SI, INPUT_PULLUP);
   pinMode(PIN_SC, OUTPUT);
   pinMode(PIN_RC, OUTPUT);
   Serial.println("End of start()");
@@ -122,39 +130,44 @@ void loop() {
     writeOutputs(~(1 << i));
     int all_bits_in_chunk = readInputs();
     for (int b = 0; b < 16; b++) {
-      uint8_t key = matrix[i][b] + 9;
+      uint8_t key = matrix[i][b];
       bool is_bit_set = (all_bits_in_chunk & (1 << b)) != 0;
       bool is_bottom_scan = i % 2 == 0;
       bool is_partial_scan = i % 2 == 1;
-
+      
       if (is_partial_scan) {
         if (is_bit_set) {
           // This key is partially pressed
-          if (micros_at_partial[key] == 0) {
+          if (micros_at_partial[key] == NOPE_ITS_RELEASED) {
             // Store micros at first instant partial press is discovered
             // For travel time (force) calculation
             micros_at_partial[key] = micros();
+            Serial.println("Partial press detected");
           }
         } else {
           // This is a key that is not even partially pressed...
           if (in_flight_velocities[key] != 0) {
             // But it is still ringing! Silence it!
             in_flight_velocities[key] = 0;
-            usbMIDI.sendNoteOff(key, 0, 0);
+            usbMIDI.sendNoteOff(key + KEYBED_OFFSET_RIGHT, 0, 0);
           }
+          micros_at_partial[key] = NOPE_ITS_RELEASED; // Clear previous partial press time
           continue; // Go to next bit no matter how you paste this around...
+          
         }
-      }
-
-      // This if-statement can never be entered on the same scan as the partial scan.
-      if (is_bottom_scan) {
+      } else if (is_bottom_scan) {
         if (is_bit_set) {
           // This key is fully pressed...
           if (in_flight_velocities[key] == 0) {
             // But it has not made any sound yet! Make some noise
-            usbMIDI.sendNoteOn(key, 100, 0); // TODO: Actually calculate velocities
-            in_flight_velocities[key] = 100;
-            Serial.println(key);
+            uint32_t partial_to_bottom_micros = micros() - micros_at_partial[key];
+            uint8_t vel = velocity_from_press_micros(partial_to_bottom_micros);
+            usbMIDI.sendNoteOn(key + KEYBED_OFFSET_RIGHT, vel, 0);
+            in_flight_velocities[key] = vel;
+            Serial.print(key);
+            Serial.print(" : ");
+            Serial.print(partial_to_bottom_micros);
+            Serial.println();
           }
         } else {
           // This key is not fully pressed
